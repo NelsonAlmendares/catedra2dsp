930fde07e593d92a54b1668836fc73cd03142fcb
NelsonAlmendares <Nelsonjosealmendares1999@gmail.com>
2024-09-24 22:06:35 -0600

    base para proyecto

---
+++ Catedra 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catedra_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
